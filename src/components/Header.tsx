2c60a31dfde6adaec40502e33ee4b451b02807e0
Fernando <fernando@inspiration-q.com>
2024-11-30 00:28:51 +0100

    Improve the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,9 @@ import { Logo } from './Logo';
 
 export function Header() {
   return (
-    <header className="fixed top-0 z-10 w-full bg-brand-dark/90 backdrop-blur">
-      <div className="mx-auto flex h-20 sm:h-16 max-w-7xl items-center px-6 sm:px-4 lg:px-8">
+    <header className="fixed top-0 z-10 w-full py-[1rem]">
+      <div className="absolute inset-0 bg-gradient-to-b from-brand-dark/[0.25] to-brand-dark/0 backdrop-blur-md"></div>
+      <div className="relative mx-auto flex h-20 sm:h-16 max-w-7xl items-center px-6 sm:px-4 lg:px-8">
         <Logo />
       </div>
     </header>
