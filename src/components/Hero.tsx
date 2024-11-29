ef53ff2f0ed6fb95cc1b50e717d5aadebb22ea6f
Fernando <fernando@inspiration-q.com>
2024-11-29 23:27:03 +0100

    Fix the heading lenght

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,12 +10,13 @@ export function Hero() {
     <Background>
       <div className="relative mx-auto min-h-screen max-w-7xl px-4 py-20 sm:px-6 lg:px-8">
         <div className="mt-24 sm:mt-20 max-w-3xl">
-          <h1 className="mb-8 text-5xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-white">
-            We are building the Strava for Motorsport.
+          <h1 className="grid mb-8 text-6xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight text-white w-full leading-[1.1]">
+            <span>We are building the</span>
+            <span>Strava for Motorsport</span>
           </h1>
-          <p className="mb-10 sm:mb-8 text-2xl sm:text-xl text-gray-300">
-            Track your performance, connect with motorsport enthusiasts, and elevate
-            your experience.
+          <p className="grid mb-10 sm:mb-8 text-2xl sm:text-xl text-gray-300 w-full">
+            <span>Track your performance, connect with motorsport enthusiasts,</span>
+            <span>and elevate your experience.</span>
           </p>
           <Button size="large" onClick={() => setIsModalOpen(true)}>
             Learn More
