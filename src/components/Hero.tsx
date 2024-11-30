9f2a505ab5b34472897f5e03ba10b59648e4fe42
Fernando <fernando@inspiration-q.com>
2024-11-30 12:46:15 +0100

    refactor: improve hero section typography and layout.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,17 +10,20 @@ export function Hero() {
     <Background>
       <div className="relative mx-auto min-h-screen max-w-7xl px-4 py-20 sm:px-6 lg:px-8">
         <div className="mt-24 sm:mt-20 max-w-3xl">
-          <h1 className="grid mb-8 text-6xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight text-white w-full leading-[1.1]">
-            <span>We are building the</span>
-            <span>Strava for Motorsport</span>
+          <h1 className="mb-8 text-6xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight text-white w-full leading-[1.1]">
+            We are building the Strava for Motorsport
           </h1>
-          <p className="grid mb-10 sm:mb-8 text-2xl sm:text-xl text-gray-300 w-full">
-            <span>Track your performance, connect with motorsport enthusiasts,</span>
-            <span>and elevate your experience.</span>
-          </p>
-          <Button size="large" onClick={() => setIsModalOpen(true)}>
-            Learn More
-          </Button>
+          <div className="max-w-[44rem]">
+            <h2 className="mb-1 sm:mb-1 text-3xl text-gray-300 w-full">
+              Track your performance, connect with motorsport enthusiasts,and elevate your experience.
+            </h2>
+            <p className="mb-10 sm:mb-8 text-2lg text-gray-400">
+              Laps stores all your track day data so you can remember, review, share, compare,<br></br> check stats and improve.
+            </p>
+            <Button size="large" onClick={() => setIsModalOpen(true)}>
+              Learn More
+            </Button>
+          </div>
         </div>
       </div>
 
